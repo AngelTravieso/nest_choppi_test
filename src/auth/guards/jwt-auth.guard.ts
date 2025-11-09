a8b695899714160a907b1a3107cb1177a5aae024
AngelTravieso <angeltraviesoc@gmail.com>
2025-11-09 18:58:47 -0400

    REFACTOR - auth guards

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
+/**
+ * Un Guard personalizado que invoca la estrategia 'jwt' de Passport.
+ *
+ * Al aplicar este Guard (ej. @UseGuards(JwtAuthGuard)) a un endpoint,
+ * NestJS ejecutará automáticamente la JwtStrategy para validar el
+ * token JWT en el encabezado de la solicitud.
+ */
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') { }
+export class JwtAuthGuard extends AuthGuard('jwt') {}
--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+/**
+ * Un Guard personalizado que invoca la estrategia 'local' de Passport.
+ *
+ * Al aplicar este Guard (ej. @UseGuards(LocalAuthGuard)) a un endpoint
+ * (como /auth/login), NestJS ejecutará automáticamente la `LocalStrategy`
+ * para validar las credenciales (email/password) del `body` de la solicitud.
+ */
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') { }
+export class LocalAuthGuard extends AuthGuard('local') {}
