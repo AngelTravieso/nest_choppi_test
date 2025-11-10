8b6918643082fc04824d7bd0490d3eba1dc252d6
AngelTravieso <angeltraviesoc@gmail.com>
2025-11-09 23:09:24 -0400

    FIX - export module services

--- src/store-products/store-product.module.ts
+++ src/store-products/store-product.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { StoreProductService } from './store-product.service';
-import { StoreProductController } from './store-product.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { StoreProduct } from './entities/store-product.entity';
-import { Store } from 'src/store/entities/store.entity';
 import { ProductModule } from 'src/product/product.module';
+import { Store } from 'src/store/entities/store.entity';
+import { StoreProduct } from './entities/store-product.entity';
+import { StoreProductController } from './store-product.controller';
+import { StoreProductService } from './store-product.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([StoreProduct, Store]), ProductModule],
